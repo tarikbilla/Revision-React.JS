8e5ab31f1c679f6e80efdcb0871747e003d2d6a8
Tarik Billa <tarikbillah@gmail.com>
2025-10-07 00:39:38 +0200

    added homepage with all modules

--- vite.config.ts
+++ vite.config.ts
@@ -3,11 +3,11 @@ import react from '@vitejs/plugin-react';
 import path from 'path'; 
 
 export default defineConfig({
+  base: "/Revision-React.JS/",
   plugins: [react()],
   resolve: {
     alias: {
       '@': path.resolve(__dirname, './src'), // Set @ to point to /src
     },
   },
-  base: "/Revision-React.JS/",
 });
